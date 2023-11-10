01682149189dcde02781f9a9c3250c6b9b57bd1f
Vinicius Martins <vinicius.martins@olx.com>
2023-11-10 21:55:09 +0000

    feature: Addiiton of dummy response on api

--- infrastructure/api/api.go
+++ infrastructure/api/api.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"github.com/ViniciusMartinss/field-team-management/application/domain"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 func CreateTaskRoutes(r *gin.Engine) {
@@ -13,6 +15,12 @@ func CreateTaskRoutes(r *gin.Engine) {
 
 func getTasks() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, "tasks")
+		currentTime := time.Now()
+		c.JSON(http.StatusOK, domain.Task{
+			ID:      1,
+			Summary: "Dummy Summary",
+			Date:    &currentTime,
+			UserID:  1,
+		})
 	}
 }
